9c912482c393649b5b59dc003792d8e802879b9c
Onriel Houston <onrielhouston@gmail.com>
2024-06-27 11:42:47 -0400

    fixing spring

---
+++ src/main/java/org/yearup/EasyshopCapstoneStarterApplication.java
@@ -0,0 +1,13 @@
+package org.yearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyshopCapstoneStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyshopCapstoneStarterApplication.class, args);
+	}
+
+}
